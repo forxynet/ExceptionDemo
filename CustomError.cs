a423bdd2ae1a21f06c8f0261600ca811d1ee22ff
forxynet <forxynet@gmail.com>
2020-06-12 22:10:04 +0300

    Exception Demo

---
+++ CustomError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo {
+    class Student {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    [Serializable]
+    class InvalidStudentNameException : SystemException {
+        public InvalidStudentNameException() {
+
+        }
+
+        public InvalidStudentNameException(string name)
+            : base(String.Format("Invalid Student Name: {0}", name)) {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo {
+    class Program {
+        static void Main(string[] args) {
+            Student newStudent = null;
+
+            try {
+                newStudent = new Student();
+                newStudent.StudentName = "James007";
+
+                ValidateStudent(newStudent);
+            }
+            catch (Exception ex) {
+
+                switch (ex.GetType().Name) {
+                    case "NotImplementedException":
+                        Console.WriteLine("NotImplementedException");
+                        break;
+                    case "System.PrinterException":
+                        Console.WriteLine("System.ArgumentNullException");
+                        break;
+                    default:
+                        Console.WriteLine("Exception");
+                        break;
+                }
+            }
+        }
+
+        private static void ValidateStudent(Student newStudent) {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
